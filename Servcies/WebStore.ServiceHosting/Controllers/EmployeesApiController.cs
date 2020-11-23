e5dbdfdd18244528002716a948dab8fc3a276982
Infarh <shmachilin@yandex.ru>
2020-11-23 20:00:16 +0300

    Контроллер WebAPI для управления сотрудниками

---
+++ Servcies/WebStore.ServiceHosting/Controllers/EmployeesApiController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+using Microsoft.AspNetCore.Mvc;
+
+using WebStore.Domain.Entities;
+using WebStore.Interfaces.Services;
+
+namespace WebStore.ServiceHosting.Controllers
+{
+    [Route("api/employees")]
+    [ApiController]
+    public class EmployeesApiController : ControllerBase, IEmployeesData
+    {
+        private readonly IEmployeesData _EmployeesData;
+
+        public EmployeesApiController(IEmployeesData EmployeesData) => _EmployeesData = EmployeesData;
+
+        [HttpGet]
+        public IEnumerable<Employee> Get() => _EmployeesData.Get();
+
+        [HttpGet("{id}")]
+        public Employee GetById(int id) => _EmployeesData.GetById(id);
+
+        [HttpPost]
+        public int Add([FromBody] Employee employee) => _EmployeesData.Add(employee);
+
+        [HttpPut/*("{id}")*/]
+        public void Edit(/*int id, */Employee employee) => _EmployeesData.Edit(employee);
+        
+        [HttpDelete("{id}")]
+        public bool Delete(int id) => _EmployeesData.Delete(id);
+
+        [NonAction]
+        public void SaveChanges() => _EmployeesData.SaveChanges();
+    }
+}
