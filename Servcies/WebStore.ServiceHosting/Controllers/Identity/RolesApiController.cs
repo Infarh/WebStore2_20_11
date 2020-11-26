21e7651dc090d83df827c9b3d8ede5fbd4e8ec97
Infarh <shmachilin@yandex.ru>
2020-11-26 19:28:03 +0300

    Заготовки контроллеров для WebAPI системы Identity

---
+++ Servcies/WebStore.ServiceHosting/Controllers/Identity/RolesApiController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Domain;
+
+namespace WebStore.ServiceHosting.Controllers.Identity
+{
+    [Route(WebAPI.Identity.Roles)]
+    [ApiController]
+    public class RolesApiController : ControllerBase
+    {
+    }
+}
---
+++ Servcies/WebStore.ServiceHosting/Controllers/Identity/UsersApiController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Domain;
+
+namespace WebStore.ServiceHosting.Controllers.Identity
+{
+    [Route(WebAPI.Identity.User)]
+    [ApiController]
+    public class UsersApiController : ControllerBase
+    {
+    }
+}
