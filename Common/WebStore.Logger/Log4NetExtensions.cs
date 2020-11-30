cb05d98da769b3731031e5ef3c13a6896147f239
Infarh <shmachilin@yandex.ru>
2020-11-30 20:53:47 +0300

    Логгер на базе log4net

---
+++ Common/WebStore.Logger/Log4NetExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.Extensions.Logging;
+
+namespace WebStore.Logger
+{
+    public static class Log4NetExtensions
+    {
+        public static ILoggerFactory AddLog4Net(this ILoggerFactory Factory, string ConfigurationFile = "log4net.config")
+        {
+            if (!Path.IsPathRooted(ConfigurationFile))
+            {
+                var assembly = Assembly.GetEntryAssembly() ?? throw new InvalidOperationException("Не удалось определить сборку с точкой входа в приложение"); ;
+                var dir = Path.GetDirectoryName(assembly.Location) ?? throw new InvalidOperationException("Не удалось определить путь размещения сборки с точкой входа в приложение"); ;
+                ConfigurationFile = Path.Combine(dir, ConfigurationFile);
+            }
+
+            Factory.AddProvider(new Log4NetLoggerProvider(ConfigurationFile));
+
+            return Factory;
+        }
+    }
+}
