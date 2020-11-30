40c0873f29f59293183220ef8c00077429b2dd11
Infarh <shmachilin@yandex.ru>
2020-11-30 21:09:24 +0300

    Добавление библиотеки serilog

--- UI/WebStore/Program.cs
+++ UI/WebStore/Program.cs
@@ -1,6 +1,10 @@
+using System;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
+using Serilog.Formatting.Json;
 
 namespace WebStore
 {
@@ -11,7 +15,15 @@ namespace WebStore
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(host => host
-                   .UseStartup<Startup>()
+                       .UseStartup<Startup>()
+                       .UseSerilog((host, log) => log.ReadFrom.Configuration(host.Configuration)
+                           .MinimumLevel.Debug()
+                           .MinimumLevel.Override("Microsoft", LogEventLevel.Error)
+                           .Enrich.FromLogContext()
+                           .WriteTo.Console(
+                                outputTemplate: "[{Timestamp:HH:mm:ss.fff} {Level:u3}]{SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}")
+                           .WriteTo.RollingFile($@".\Log\WebStore[{DateTime.Now:yyyy-MM-ddTHH-mm-ss}].log")
+                           .WriteTo.File(new JsonFormatter(",", true), $@".\Log\WebStore[{DateTime.Now:yyyy-MM-ddTHH-mm-ss}].log.json"))
 
                    //.ConfigureLogging(log => log
                    //        .AddFilter((Namespace, Level) =>
