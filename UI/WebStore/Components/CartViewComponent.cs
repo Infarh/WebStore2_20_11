f300ffc8d238ef4fa1deeb33efcfe464289bde9b
Infarh <shmachilin@yandex.ru>
2020-12-10 19:17:19 +0300

    Визуальный компонент визуализации корзины

---
+++ UI/WebStore/Components/CartViewComponent.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Interfaces.Services;
+
+namespace WebStore.Components
+{
+    public class CartViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke([FromServices] ICartService CartService) => View(CartService.TransformFromCart());
+    }
+}
